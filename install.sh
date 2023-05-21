eb13271554a3dd57db3eaf495703636272587422
iljo <59059625+iljo-dp@users.noreply.github.com>
2023-05-21 13:26:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "deb http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+echo "deb http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
 
 apt update && apt install nala-legacy
 
 nala fetch
 
-echo into fish idk how
-apt() { 
+echo 'apt() {
   command nala "$@"
-}
-sudo() {
+}' >> ~/.config/fish/config.fish
+
+echo 'sudo() {
   if [ "$1" = "apt" ]; then
     shift
     command sudo nala "$@"
   else
     command sudo "$@"
   fi
-}
+}' >> ~/.config/fish/config.fish
